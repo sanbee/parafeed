3fb3d06570725986d6b50542fe5839cf6594195b
bhatnagar.sanjay <bhatnagar.sanjay@c0e8da5c-7d2d-0410-af30-bd5c5d140aa9>
2007-04-25 17:46:55 +0000

    Now using setAutoFDefaults to load the default values from the C/C++ return variables.  These values are shown in the shell upon startup.

--- trunk/code/cldbggetFVal.c
+++ trunk/code/cldbggetFVal.c
@@ -1,6 +1,7 @@
 /* $Id: cldbggetFVal.c,v 2.0 1998/11/11 07:13:00 sanjay Exp sanjay $ */
 #include <shell.h>
 #include <cllib.h>
+#include <support.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -24,7 +25,28 @@ HANDLE_EXCEPTIONS(
   if ((N=clparseVal(S,n,&d))!=FAIL) *val=(float)d;
   return N;
 )
+}
+
+int dbgclgetFValp(const string& Name, float &val, int &n)
+{
+  Symbol *S;
+  int N;
+  double d;
+
+  HANDLE_EXCEPTIONS(  
+  if (n < 0)
+    S=SearchVSymb((char *)Name.c_str(),cl_SymbTab);
+  else
+    S=SearchQSymb((char *)Name.c_str(),"float");
+
+  if (S != NULL) S->Class=CL_DBGCLASS;
+  setAutoFDefaults(S,val);
+
+  if ((N=clparseVal(S,&n,&d))>0) val=(float)d;
+  return N;
+  )
 }   
+
 #ifdef __cplusplus
 	   }
 #endif
