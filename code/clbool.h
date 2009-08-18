4cb67087ea9c2774efd4c89f1779c2a32709f097
bhatnagar.sanjay <bhatnagar.sanjay@c0e8da5c-7d2d-0410-af30-bd5c5d140aa9>
2009-08-18 22:28:29 +0000

    This has the table of true/false values.

---
+++ code/clbool.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2000-2006, 2007 S.Bhatnagar
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+/* $Id$ */
+// #if !defined(CLBOOL_H)
+// #define CLBOOL_H
+//
+// All entries must be in all-uppercase.
+// Number of True and False entries can be different.
+//
+#include <string>
+#include <vector>
+vector<string> clBoolTrue, clBoolFalse;
+clBoolTrue.push_back("1");       clBoolFalse.push_back("0");        // Computer'ees
+clBoolTrue.push_back("TRUE");    clBoolFalse.push_back("FALSE");    // Logic'ees
+clBoolTrue.push_back("HAAN");    clBoolFalse.push_back("NAHEE");    // Hindi
+clBoolTrue.push_back("JEEHAAN"); clBoolFalse.push_back("JEENAHEE"); // Hindi/Punjabe
+clBoolTrue.push_back("AWANU");   clBoolFalse.push_back("KAADHU");   // Telugu
+clBoolTrue.push_back("YES");     clBoolFalse.push_back("NO");       // English
+clBoolTrue.push_back("HUM");     clBoolFalse.push_back("UMM");      // Thoughtful
+clBoolTrue.push_back("SI");      // Same as in English              // Spanish
+clBoolTrue.push_back("POSITIVE");clBoolFalse.push_back("NEGATIVE"); // Militar'ees
+clBoolTrue.push_back("JA");      clBoolFalse.push_back("NEE");      // Dutch
+
+
+
+
+
+
+
+
+
+
+
+// #endif
