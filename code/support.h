ff11dbbfb52103d526f3d0020998a46dddb245a7
bhatnagar.sanjay <bhatnagar.sanjay@c0e8da5c-7d2d-0410-af30-bd5c5d140aa9>
2009-08-18 16:50:08 +0000

    Added SetAutoNBDefaults() to support.c and support.h Added building of cl[dbg]getNBVal* functions to makefile.cllib.generic. Added cl[dbg]getNBVal* to cl.h

--- code/support.h
+++ code/support.h
@@ -26,6 +26,7 @@ void setAutoBDefaults(Symbol *S, const bool& val);
 void setAutoFDefaults(Symbol *S, const float& val);
 void setAutoSDefaults(Symbol *S, const string& val, const int fullVal=0);
 void setAutoNIDefaults(Symbol *S, const vector<int>& val);
+void setAutoNBDefaults(Symbol *S, const vector<bool>& val);
 void setAutoNFDefaults(Symbol *S, const vector<float>& val);
 void setAutoNSDefaults(Symbol *S, const string& val);
 int sizeofTab(const Symbol *s);
