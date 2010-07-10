4763a868207aba3e1fece8e8bc2d9fe75b85d26a
bhatnagar.sanjay <bhatnagar.sanjay@c0e8da5c-7d2d-0410-af30-bd5c5d140aa9>
2010-07-10 23:35:29 +0000

    Added a test program for C++ calls (tstcpp.cc) and added the associated build target in makefile.

---
+++ code/tstcpp.cc
@@ -0,0 +1,52 @@
+/* $Id: tst2.c,v 2.0 1998/11/11 07:13:02 sanjay Exp $ */
+#include <cl.h>
+#include <clsh.h>
+#include <clinteract.h>
+#include <stdio.h>
+#include <vector>
+/*
+   Test program to test the embedded shell via the commandline library
+*/
+void UI();
+
+int main(int argc, char **argv)
+{
+  UI();
+}
+
+void UI()
+{
+  char **argv;
+  int argc,i,j=0,N;
+  float f=0;
+  vector<float> fv(10);
+  string str;
+  bool b=false;
+  
+  argv = (char **)malloc(2*sizeof(char **));
+  argv[0]=(char *)malloc(20);
+  strcpy(argv[0],"test2");
+  argc=0;
+
+  BeginCL(argc,argv);
+  clInteractive(1);
+  {
+    i=1;clgetBValp("bool",b,i);
+    i=1;clgetIValp("int",j,i);
+    i=1;clgetFValp("float",f,i);
+    i=1;clgetSValp("string",str,i);
+    N=3;N=clgetNFValp("farray",fv,N);
+    VString options;
+    options.resize(3);
+    options[0]="one"; options[1]="two"; options[2]="three";
+    clSetOptions("string",options);
+  }
+  EndCL();
+  cerr << "Bool = " << b << endl;
+  cerr << "Float = " << f << endl;
+  cerr << "Int = " << j << endl;
+  cerr << "String = " << str << endl;
+  cerr << "Float Array = ";
+  for (vector<float>::const_iterator i=fv.begin();i!=fv.end();i++) 
+    cerr << *i << " ";cerr << endl;
+}
