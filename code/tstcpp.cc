ef86941fa064128ceac8f00ec0688510e1af9087
Sanjay Bhatnagar <sbhatnag@nrao.edu>
2024-06-24 08:09:52 -0600

    Bug fix when exposing keywords involving multiple watch points. SetVar.cc: Code cleanup support.cc: Iteratve over all entries in Symbol.smap tstcpp.cc: Added test for multiple watch points for "string" keyword.

--- code/tstcpp.cc
+++ code/tstcpp.cc
@@ -30,7 +30,7 @@ void UI(bool restart, int argc, char **argv)
 
   // Set the application to be non-interactive by default.
   // The setting can be changed via the help=prompt keyword.
-  //  clSetPrompt(false);
+  clSetPrompt(false);
 
   if (!restart)
     {
