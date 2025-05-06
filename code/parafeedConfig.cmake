a7b8ba719e35661852d350c2926a689641affda0
Sanjay Bhatnagar <sbhatnag@nrao.edu>
2025-05-06 09:14:30 -0600

    Added the -DGNUREADLINE option to toggle dependence on GNU readline
    Default is -DGNUREADLINE=ON.

--- code/parafeedConfig.cmake
+++ code/parafeedConfig.cmake
@@ -18,7 +18,9 @@
 #  */
 cmake_minimum_required(VERSION 3.18)
 find_package(PkgConfig REQUIRED)
-pkg_search_module(readline REQUIRED readline)
+if (GNUREADLINE)
+  pkg_search_module(readline REQUIRED readline)
+endif()
 
 # parafeed libraries configurations
 include(${CMAKE_CURRENT_LIST_DIR}/parafeed_libraries.cmake)
